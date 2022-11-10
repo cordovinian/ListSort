e2d33ed054762f584983f980e9c4f3afbf9d182f
cordovinian <schmidt.matt@gmail.com>
2022-11-09 17:56:19 -0800

    Add quick sort function

--- ListSortingDemo.cs
+++ ListSortingDemo.cs
@@ -12,6 +12,14 @@ internal class ListSortingDemo
         Console.WriteLine("Easiest solution using LINQ built-ins:");
         Console.WriteLine(string.Join(", ", sourceList.Distinct().Order()));
 
+        Console.WriteLine("Use the datatype SortedSet to order and de-duplicate:");
+        var listAsSortedSet = new SortedSet<int>();
+        foreach (var item in sourceList)
+        {
+            listAsSortedSet.Add(item);
+        }
+        Console.WriteLine(string.Join(", ", listAsSortedSet));
+
         Console.WriteLine("Use a HashSet to de-duplicate:");
         var listAsHash = new HashSet<int>();
         foreach (var item in sourceList)
@@ -20,12 +28,45 @@ internal class ListSortingDemo
         }
         Console.WriteLine(string.Join(", ", listAsHash));
 
-        Console.WriteLine("Use a SortedSet to order and de-duplicate:");
-        var listAsSortedSet = new SortedSet<int>();
-        foreach (var item in sourceList)
+        Console.WriteLine("Use quick sort to order:");
+        List<int> result = QuickSortList(listAsHash.ToList(), 0, listAsHash.Count - 1);
+        Console.WriteLine(string.Join(", ", result));
+    }
+
+    public static List<int> QuickSortList(List<int> sourceList, int leftIndex, int rightIndex)
+    {
+        var pivot = sourceList[leftIndex];
+        var left = leftIndex;
+        var right = rightIndex;
+
+        while (left <= right)
         {
-            listAsSortedSet.Add(item);
+            while (sourceList[left] < pivot)
+            {
+                left++;
+            }
+            while (sourceList[right] > pivot)
+            {
+                right--;
+            }
+            if (left <= right)
+            {
+                int swap = sourceList[left];
+                sourceList[left] = sourceList[right];
+                sourceList[right] = swap;
+                left++;
+                right--;
+            }
         }
-        Console.WriteLine(string.Join(", ", listAsSortedSet));
+        if (leftIndex < right)
+        {
+            QuickSortList(sourceList, leftIndex, right);
+        }
+        if (left < rightIndex)
+        {
+            QuickSortList(sourceList, left, rightIndex);
+        }
+
+        return sourceList;
     }
 }
