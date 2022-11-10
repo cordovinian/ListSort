e4cbb73174e57195e4b50095f72c918436cd33b6
cordovinian <schmidt.matt@gmail.com>
2022-11-09 17:24:11 -0800

    Demo sort and de-duplicate with SortedSet

--- ListSortingDemo.cs
+++ ListSortingDemo.cs
@@ -12,15 +12,20 @@ internal class ListSortingDemo
         Console.WriteLine("Easiest solution using LINQ built-ins:");
         Console.WriteLine(string.Join(", ", sourceList.Distinct().Order()));
 
-        Console.WriteLine("Hash based solution to de-duplicate:");
+        Console.WriteLine("Use a HashSet to de-duplicate:");
         var listAsHash = new HashSet<int>();
         foreach (var item in sourceList)
         {
             listAsHash.Add(item);
         }
-        foreach (var item in listAsHash)
+        Console.WriteLine(string.Join(", ", listAsHash));
+
+        Console.WriteLine("Use a SortedSet to order and de-duplicate:");
+        var listAsSortedSet = new SortedSet<int>();
+        foreach (var item in sourceList)
         {
-            Console.Write("{0}, ", item);
+            listAsSortedSet.Add(item);
         }
+        Console.WriteLine(string.Join(", ", listAsSortedSet));
     }
 }
