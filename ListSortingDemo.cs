11f9db15afd52874d81c6829b8dd78f2885cf0f9
cordovinian <schmidt.matt@gmail.com>
2022-11-09 17:11:00 -0800

    Demo de-duplicate with hash

--- ListSortingDemo.cs
+++ ListSortingDemo.cs
@@ -6,12 +6,21 @@ internal class ListSortingDemo
     {
         var sourceList = new List<int> { -1, 4, 3, 2, 1, 1, 2, 3, 4 };
 
-        Console.WriteLine("Easiest solution using LINQ built-ins");
-
         Console.WriteLine("Source list:");
-        Console.WriteLine(string.Join(",", sourceList));
+        Console.WriteLine(string.Join(", ", sourceList));
 
-        Console.WriteLine("Resulting list:");
-        Console.WriteLine(string.Join(",", sourceList.Distinct().Order()));
+        Console.WriteLine("Easiest solution using LINQ built-ins:");
+        Console.WriteLine(string.Join(", ", sourceList.Distinct().Order()));
+
+        Console.WriteLine("Hash based solution to de-duplicate:");
+        var listAsHash = new HashSet<int>();
+        foreach (var item in sourceList)
+        {
+            listAsHash.Add(item);
+        }
+        foreach (var item in listAsHash)
+        {
+            Console.Write("{0}, ", item);
+        }
     }
 }
